a1500ce99fa898ae9cf528c1d959345f2b6ba8d5
Shukazuby <shukazuby@gmail.com>
2025-03-26 15:44:34 +0100

    revert

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,5 +3,4 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 export const DB_NAME = process.env.DB_NAME || "test";
-// export const DB_CONNECTION_STRING = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@db:27017/${DB_NAME}?authSource=admin`;
-export const DB_CONNECTION_STRING = `${process.env.DEV_MONGOURI}`;
+export const DB_CONNECTION_STRING = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@db:27017/${DB_NAME}?authSource=admin`;
