776feeabe7e871594317191a76c410a61d67c4f6
mdauwal <pricelezzchap@gmail.com>
2025-03-26 14:19:03 +0000

    feat: Implemented CRUD for Subscription Model

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import authRouter from './auth.router';
 import userRouter from './user.router';
+import subscriptionRouter from './subscription.router';
+
 
 const router = express.Router();
 export default (): express.Router => {
   authRouter(router);
   userRouter(router);
+  subscriptionRouter(router);
   return router;
 };
