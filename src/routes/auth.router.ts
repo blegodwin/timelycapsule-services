e5344ac4f1e6488033d039c95425cc06fdadcc86
cyphoriatech <cyphoriatech@gmail.com>
2025-03-26 10:38:26 +0100

    Implemented Crud Operations for Notifcation

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Auth } from "../controllers/auth.controller";
 
 const authRouter = (router: express.Router) => {
+  router.use(express.json());
   router.post("/login", Auth.login);
   router.post("/resendemail", Auth.resendVerificationEmail);
   router.post("/register", Auth.register);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRouter from './auth.router';
 import userRouter from './user.router';
+import notificationRouter from './notification.router';
 
 const router = express.Router();
 export default (): express.Router => {
   authRouter(router);
   userRouter(router);
+  notificationRouter(router);
   return router;
 };
