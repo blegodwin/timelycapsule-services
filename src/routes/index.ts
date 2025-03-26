3deb1bea228a24a5c6975446dc124e3b9743ce27
portableDD <emmanueltemitopedorcas20@gmail.com>
2025-03-26 23:23:41 +0100

    Referral-api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import authRouter from './auth.router';
 import userRouter from './user.router';
 import streakRouter from './streak.router';
+import referralRouter from './referral-router';
 
 const router = express.Router();
 export default (): express.Router => {
   authRouter(router);
   userRouter(router);
-  streakRouter(router)
+  streakRouter(router);
+  referralRouter(router);
   return router;
 };
