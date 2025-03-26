1fe1c28dc5298e71662683de882d4c700767fc27
nafiuishaaq <nafiuig@gmail.com>
2025-03-26 14:04:20 +0100

    implemented AI Content model with CRUD operations

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRouter from './auth.router';
 import userRouter from './user.router';
+import aiContentRouter from './ai-content.router';
 
 const router = express.Router();
 export default (): express.Router => {
   authRouter(router);
   userRouter(router);
+  aiContentRouter(router);
   return router;
 };
