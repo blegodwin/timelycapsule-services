7705d428f45f572e4f967d89464423fd25e95c74
Assad Isah <nottherealalanturing@gmail.com>
2025-03-26 12:26:51 +0100

    cleanups

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import authRouter from './auth.router';
 import userRouter from './user.router';
-import aiContentRouter from './aiContent.router';
 
 const router = express.Router();
 export default (): express.Router => {
   authRouter(router);
   userRouter(router);
-  aiContentRouter(router);
   return router;
 };
