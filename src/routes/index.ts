717b5bd53218a262b3c86b6ffa3aed40e948e60e
TInna23 <tylermalcom31@gmail.com>
2025-03-27 09:50:28 +0100

    Implement Report Endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
-import express from 'express';
-import authRouter from './auth.router';
-import userRouter from './user.router';
+import express from "express";
+import authRouter from "./auth.router";
+import userRouter from "./user.router";
+import analyticRouter from "./analytic.router";
 
 const router = express.Router();
 export default (): express.Router => {
   authRouter(router);
   userRouter(router);
+  //analyticRouter(router);
   return router;
 };
