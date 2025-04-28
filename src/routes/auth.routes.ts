d4f627823747b216bd5e25915c0f7cf621089f0e
clintjeff2 <clintjeff2@gmail.com>
2025-04-28 18:05:55 +0100

    Added logging middleware to route /guest for testing and showing example proper usage of logging middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { register, login, logout, guest } from '../controllers/auth.controller';
 import { registerValidation, loginValidation } from '../utils/validators';
+import { logActivity } from '../middleware/activityLog';
 
 const router = Router();
 
 export default (): Router => {
-  router.post('/register', registerValidation, register);
-  router.post('/login', loginValidation, login);
-  router.post('/logout', logout);
-  router.post('/guest', guest);
-  return router;
+	router.post('/register', registerValidation, register);
+	router.post('/login', loginValidation, login);
+	router.post('/logout', logout);
+	router.post('/guest', logActivity('guest_session_start'), guest);
+	return router;
 };
