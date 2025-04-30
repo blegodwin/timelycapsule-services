490b26aa503a44feb1dadc52222d8f13175f8852
phertyameen <aminubabafatima8@gmail.com>
2025-04-30 01:23:53 +0100

    feat(capsule): create SecretDrop capsule with validation, sanitization & auth

---
+++ src/routes/secretDrop.router.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import controller from '../controllers/secretDrop/secretDrop.controller';
+
+const secretDropRouter = (router: express.Router) => {
+  router.get('/', controller.getCapsule);
+  router.put('/:id', controller.getCapsule);
+  router.delete('/:id', controller.getCapsule);
+  router.create('/:id', controller.getCapsule);
+};
+
+export default secretDropRouter;
